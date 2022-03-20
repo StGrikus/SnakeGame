0a8c63173803f9acc6f02a509932cbda133ae85b
Arthur <saintgrikus@gmail.com>
2022-03-20 22:23:07 +0200

    create public class GameField extends JPanel

--- src/GameField.java
+++ src/GameField.java
@@ -1,12 +1,30 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GameField extends JPanel {
     private final int SIZE = 320;
     private final int DOT_SIZE = 16;
-
+    private final int ALL_DOTS = 400;
+    private Image dot;
+    private Image apple;
+    private int appleX;
+    private int appleY;
+    private int[] x = new int[ALL_DOTS];
+    private int[] y = new int[ALL_DOTS];
+    private int dots;
+    private Timer timer;
+    private boolean left = false;
+    private  boolean right = true;
+    private boolean up = false;
+    private boolean down = false;
+    private boolean inGame = true;
 
 
     public GameField(){
+        setBackground(Color.BLACK);
+    }
+
+    public void loadImages(){
 
     }
 }
